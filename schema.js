f5a271ab465a641d5492a96c9c7b8aff41028de1
bildad namawa <bildadnamawa@gmail.com>
2018-12-29 00:09:26 +0300

    added graphiql

---
+++ schema.js
@@ -0,0 +1,5 @@
+import { buildSchema } from  'graphql';
+
+const schema = buildSchema(` type Query { hello: String } `);
+
+export default schema;
--- server.js
+++ server.js
@@ -1,9 +1,19 @@
-import express from 'express'
+import express from 'express';
+import graphqlHTTP from 'express-graphql';
+import schema from './schema';
+
 
 const app = express();
 
 app.get('/', (req, res) => {
     res.send("GraphQL and Relay modern is kinda cool!")
 });
+
+const root = {hello: () => "Hi, I`m Billy!"}
+
+app.use('/graphql', graphqlHTTP({
+    schema, rootValue: root,
+    graphiql: true,
+}))
 
 app.listen(9000, () => console.log('Running server on localhost:9000/graphql'))
