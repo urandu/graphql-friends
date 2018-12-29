63270b9de7c907916dcc54d1bdaffbee48aa26e4
bildad namawa <bildadnamawa@gmail.com>
2018-12-29 12:36:04 +0300

    added graphql and relay server

--- server.js
+++ server.js
@@ -1,50 +1,51 @@
 import express from 'express';
 import graphqlHTTP from 'express-graphql';
 import schema from './schema';
+import path from 'path';
+import webpack from 'webpack';
+import WebPackDevServer from 'webpack-dev-server';
+import {schema} from './data/database'; // todo
+
+const APP_PORT = 3000;
+
+const GRAPHQL_PORT = 9000;
+
+const graphQLServer = express();
+
+// GraphQL Server
+graphQLServer.use('/', graphqlHTTP({
+    schema: schema,
+    pretty: true,
+    graphiql: true,
+}));
+
+graphQLServer.listen(GRAPHQL_PORT, () => console.log(`GraphQL server on localhost:${GRAPHQL_PORT}`))
 
 
-const app = express();
+// Relay
 
-app.get('/', (req, res) => {
-    res.send("GraphQL and Relay modern is kinda cool!")
+const compiler = webpack({
+    entry: ['whatwg-fetch', path.resolve(__dirname, 'src', 'App.js')],
+    module: {
+        loaders:[
+            {
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+                test: /\.js$/,
+            },
+        ],
+    },
+    output: {filename: 'App.js', path: '/'}
 });
 
-class Friend{
-    constructor(id, {firstName, lastName, gender, language, email}){
-        this.id = id;
-        this.firstName = firstName;
-        this.lastName =lastName ;
-        this.gender = gender;
-        this.language = language;
-        this.email = email;
-    }
-}
+const app = new WebPackDevServer(compiler, {
+    contentBase: '/public/',
+    proxy: {'/graphql': `http://localhost:${APP_PORT}`},
+    publicPath: '/src/',
+    stats: {colors: true},
+});
 
-const friendDatabase = {};
-
-
-
-const global = {
-    getFriend: ({id}) => {
-        return new Friend(id, friendDatabase[id]);
-    },
-
-    createFriend: ({input}) => {
-        let id = require('crypto').randomBytes(10).toString('hex');
-        friendDatabase[id] = input;
-        return new Friend(id, input)
-    },
-
-    updateFriend: ({id, input}) => {
-        friendDatabase[id] = input;
-        return new Friend(id, input)
-    }
-
-};
-
-app.use('/graphql', graphqlHTTP({
-    schema, rootValue: global,
-    graphiql: true,
-}))
-
-app.listen(9000, () => console.log('Running server on localhost:9000/graphql'))
+app.use('/', express.static(path.resolve(__dirname, 'public')));
+app.listen(APP_PORT, ()=>{
+    console.log(`App server on localhost:${APP_PORT}`)
+});
