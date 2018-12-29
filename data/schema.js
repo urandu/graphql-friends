cccbed0faabf13732a6bd32c50b9e507a0481111
bildad namawa <bildadnamawa@gmail.com>
2018-12-29 15:42:22 +0300

    added relay database and necessary scripts

---
+++ data/schema.js
@@ -0,0 +1,106 @@
+import {
+    GraphQLObjectType,
+    GraphQLSchema,
+    GraphQLString,
+} from 'graphql';
+
+import {
+    connectionArgs,
+    connectionDefinitions,
+    connectionFromArray,
+    fromGlobalId,
+    nodeDefinitions,
+    toGlobalId,
+} from 'graphql-relay';
+
+import {
+    User,
+    Friend,
+    getFriend,
+    getFriends,
+    getUser,
+    getViewer,
+} from './database';
+
+
+const {nodeInterface, nodeField} = nodeDefinitions(
+    (globalId) =>
+    {
+        const {type, id} = fromGlobalId(globalId);
+        if (type === 'Friend'){
+            return getFriend(id);
+        }else if(type === 'User'){
+            return getUser(id);
+        }
+        return null
+    },
+    (obj) => {
+        if(obj instanceof Friend){
+            return GraphQLFriend;
+        }else if (obj instanceof User){
+            return GraphQLUser;
+        }
+        return null;
+    }
+);
+
+const GraphQLUser = new GraphQLObjectType({
+    name: 'User',
+    fields: () => ({
+        id: globalIdField('User'),
+        friends: {
+            type: friendsConnection,
+            args: connectionArgs,
+            resolve: (_, args) => connectionFromArray(getFriends(), args),
+        }
+    }),
+    interface: [nodeInterface]
+});
+
+
+const GraphQLFriend = new GraphQLObjectType({
+    name: 'User',
+    fields: () => ({
+        id: globalIdField('Friend'),
+        firstName: {
+            type: GraphQLString,
+        },
+        lastName: {
+            type: GraphQLString,
+        },
+        language: {
+            type: GraphQLString,
+        },
+        gender: {
+            type: GraphQLString,
+        },
+        email: {
+            type: GraphQLString,
+        },
+        image: {
+            type: GraphQLString,
+        },
+    }),
+    interface: [nodeInterface],
+});
+
+
+const {connectionType: friendsConnection} =
+    connectionDefinitions({name: 'Friend', nodeType: GraphQLFriend});
+
+
+const Query = new GraphQLObjectType({
+    name:'Query',
+    fields: () => ({
+        node: nodeField,
+            viewer: {
+            type:GraphQLUser,
+                resolve:() => getViewer(),
+            }
+    })
+});
+
+export const schema = new GraphQLSchema({
+    query:Query,
+})
+
