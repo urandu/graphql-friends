270919b3a26a948f16e7c11f82682ebec7398208
bildad namawa <bildadnamawa@gmail.com>
2018-12-29 23:18:20 +0300

    create environment network

--- src/App.js
+++ src/App.js
@@ -1,8 +1,33 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import {QueryRenderer, graphql} from 'react-relay'
+import {Environment, Network, RecordSource, Store } from 'relay-runtime'
 
 
 import Friends from './components/Friends'
+
+// Network layer
+function fetchQuery(operation, variables){
+    return fetch('/graphql', {
+        method:'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            query: operation.text,
+            variables
+        }),
+    }).then(response => {
+        return response.json()
+    });
+}
+
+//Relay environment
+
+const modernEnvironment = new Environment({
+    network: Network.create(fetchQuery),
+    store: new Store(new RecordSource()),
+});
 
 const mountNode = document.getElementById('root');
 
